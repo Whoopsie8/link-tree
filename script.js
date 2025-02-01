cca0ff633fce297934472ddaf95e2e84ec8e90cc
Whoopsie8 <stewart8smith@gmail.com>
2025-02-01 22:32:16 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var Fire = function () {
   this.aSpark = [];
   this.aSpark2 = [];
 
-  this.mouse = {
+  this.center = {
     x: this.canvas.width * 0.5,
     y: this.canvas.height * 0.75,
   };
@@ -17,7 +17,6 @@ var Fire = function () {
 };
 
 Fire.prototype.init = function () {
-  this.canvas.addEventListener('mousemove', this.updateMouse.bind(this), false);
   this.imageObj = new Image();
   this.imageObj.src = 'https://assets.codepen.io/13471/silver-glitter-background.png';
   this.pattern = this.ctx.createPattern(this.imageObj, 'repeat');
@@ -40,21 +39,47 @@ Fire.prototype.stop = function () {
 };
 
 Fire.prototype.update = function () {
-  this.aFires.push(new Flame(this.mouse));
-  this.aSpark.push(new Spark(this.mouse));
-  this.aSpark2.push(new Spark(this.mouse));
+  // Spawn flames around the border
+  if (this.aFires.length < 100) {
+    const side = Math.floor(rand(0, 4)); // 0: top, 1: right, 2: bottom, 3: left
+    let x, y;
 
-  for (var i = this.aFires.length - 1; i >= 0; i--) {
+    switch (side) {
+      case 0: // Top
+        x = rand(0, this.canvas.width);
+        y = 0;
+        break;
+      case 1: // Right
+        x = this.canvas.width;
+        y = rand(0, this.canvas.height);
+        break;
+      case 2: // Bottom
+        x = rand(0, this.canvas.width);
+        y = this.canvas.height;
+        break;
+      case 3: // Left
+        x = 0;
+        y = rand(0, this.canvas.height);
+        break;
+    }
+
+    this.aFires.push(new Flame({ x, y }, this.center));
+  }
+
+  this.aSpark.push(new Spark(this.center));
+  this.aSpark2.push(new Spark(this.center));
+
+  for (let i = this.aFires.length - 1; i >= 0; i--) {
     if (this.aFires[i].alive) this.aFires[i].update();
     else this.aFires.splice(i, 1);
   }
 
-  for (var i = this.aSpark.length - 1; i >= 0; i--) {
+  for (let i = this.aSpark.length - 1; i >= 0; i--) {
     if (this.aSpark[i].alive) this.aSpark[i].update();
     else this.aSpark.splice(i, 1);
   }
 
-  for (var i = this.aSpark2.length - 1; i >= 0; i--) {
+  for (let i = this.aSpark2.length - 1; i >= 0; i--) {
     if (this.aSpark2[i].alive) this.aSpark2[i].update();
     else this.aSpark2.splice(i, 1);
   }
@@ -66,26 +91,21 @@ Fire.prototype.draw = function () {
 
   this.ctx.globalCompositeOperation = 'overlay';
 
-  for (var i = this.aFires.length - 1; i >= 0; i--) {
+  for (let i = this.aFires.length - 1; i >= 0; i--) {
     this.aFires[i].draw(this.ctx);
   }
 
   this.ctx.globalCompositeOperation = 'soft-light';
 
-  for (var i = this.aSpark.length - 1; i >= 0; i--) {
+  for (let i = this.aSpark.length - 1; i >= 0; i--) {
     if (i % 2 === 0) this.aSpark[i].draw(this.ctx);
   }
 
   this.ctx.globalCompositeOperation = 'color-dodge';
 
-  for (var i = this.aSpark2.length - 1; i >= 0; i--) {
+  for (let i = this.aSpark2.length - 1; i >= 0; i--) {
     this.aSpark2[i].draw(this.ctx);
   }
-};
-
-Fire.prototype.updateMouse = function (e) {
-  this.mouse.x = e.clientX;
-  this.mouse.y = e.clientY;
 };
 
 Fire.prototype.clearCanvas = function () {
@@ -100,22 +120,29 @@ Fire.prototype.clearCanvas = function () {
 };
 
 Fire.prototype.drawHalo = function () {
-  var r = rand(300, 350);
+  const r = rand(300, 350);
   this.ctx.globalCompositeOperation = 'lighter';
-  this.grd = this.ctx.createRadialGradient(this.mouse.x, this.mouse.y, r, this.mouse.x, this.mouse.y, 0);
+  this.grd = this.ctx.createRadialGradient(
+    this.center.x,
+    this.center.y,
+    r,
+    this.center.x,
+    this.center.y,
+    0
+  );
   this.grd.addColorStop(0, 'transparent');
   this.grd.addColorStop(1, 'rgb(50, 2, 0)');
   this.ctx.beginPath();
-  this.ctx.arc(this.mouse.x, this.mouse.y - 100, r, 0, 2 * Math.PI);
+  this.ctx.arc(this.center.x, this.center.y - 100, r, 0, 2 * Math.PI);
   this.ctx.fillStyle = this.grd;
   this.ctx.fill();
 };
 
-var Flame = function (mouse) {
-  this.cx = mouse.x;
-  this.cy = mouse.y;
-  this.x = rand(this.cx - 25, this.cx + 25);
-  this.y = rand(this.cy - 5, this.cy + 5);
+var Flame = function (startPos, targetPos) {
+  this.startPos = startPos;
+  this.targetPos = targetPos;
+  this.x = startPos.x;
+  this.y = startPos.y;
   this.lx = this.x;
   this.ly = this.y;
   this.vy = rand(1, 3);
@@ -136,23 +163,21 @@ Flame.prototype.update = function () {
   this.lx = this.x;
   this.ly = this.y;
 
-  this.y -= this.vy;
-  this.vy += 0.08;
+  // Move toward the target position
+  const dx = this.targetPos.x - this.x;
+  const dy = this.targetPos.y - this.y;
+  const angle = Math.atan2(dy, dx);
 
-  this.x += this.vx;
+  this.x += Math.cos(angle) * 2;
+  this.y += Math.sin(angle) * 2;
 
-  if (this.x < this.cx) this.vx += 0.2;
-  else this.vx -= 0.2;
-
-  if (this.r > 0) this.r -= 0.3;
-
+  this.r -= 0.3;
   if (this.r <= 0) this.r = 0;
 
   this.life -= 0.12;
 
   if (this.life <= 0) {
     this.c.a -= 0.05;
-
     if (this.c.a <= 0) this.alive = false;
   } else if (this.life > 0 && this.c.a < this.c.ta) {
     this.c.a += 0.08;
@@ -188,11 +213,10 @@ Flame.prototype.draw = function (ctx) {
   ctx.closePath();
 };
 
-var Spark = function (mouse) {
-  this.cx = mouse.x;
-  this.cy = mouse.y;
-  this.x = rand(this.cx - 40, this.cx + 40);
-  this.y = rand(this.cy, this.cy + 5);
+var Spark = function (targetPos) {
+  this.targetPos = targetPos;
+  this.x = rand(targetPos.x - 40, targetPos.x + 40);
+  this.y = rand(targetPos.y, targetPos.y + 5);
   this.lx = this.x;
   this.ly = this.y;
   this.vy = rand(1, 3);
@@ -215,7 +239,7 @@ Spark.prototype.update = function () {
   this.y -= this.vy;
   this.x += this.vx;
 
-  if (this.x < this.cx) this.vx += 0.2;
+  if (this.x < this.targetPos.x) this.vx += 0.2;
   else this.vx -= 0.2;
 
   this.vy += 0.08;
@@ -223,7 +247,6 @@ Spark.prototype.update = function () {
 
   if (this.life <= 0) {
     this.c.a -= 0.05;
-
     if (this.c.a <= 0) this.alive = false;
   }
 };
